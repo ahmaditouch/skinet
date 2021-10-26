8368c4c289f1d3e96a784dd303fd9b40a5206cfe
ahmaditouch <madiwhach.fia@gmail.com>
2021-10-26 12:11:46 +0000

    identity jwt authentication

--- API/Controllers/BuggyController.cs
+++ API/Controllers/BuggyController.cs
@@ -1,5 +1,6 @@
 using API.Errors;
 using Infrastructure.Data;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
@@ -10,6 +11,13 @@ namespace API.Controllers
         public BuggyController(StoreContext context)
         {
             _context = context;
+        }
+
+        [HttpGet("testauth")]
+        [Authorize]
+        public ActionResult<string> GetSecretText()
+        {
+            return "secret stuff";
         }
 
         [HttpGet("notfound")]
@@ -27,7 +35,7 @@ namespace API.Controllers
         {
             var thing = _context.Products.Find(42);
 
-            var thingToReturn = thing.ToString(); //  note null.tostring
+            var thingToReturn = thing.ToString();
 
             return Ok();
         }
--- API/Startup.cs
+++ API/Startup.cs
@@ -9,6 +9,7 @@ using API.Middleware;
 using AutoMapper;
 using Core.Interfaces;
 using Infrastructure.Data;
+using Infrastructure.Identity;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -42,13 +43,19 @@ namespace API
             services.AddDbContext<StoreContext>(x =>
             x.UseSqlite(_config.GetConnectionString("DefaultConnection")));
             // x.UseSqlite("Data Source=skinet.db"));
-             services.AddSingleton<IConnectionMultiplexer>(c =>
-            {
-                var configuration = ConfigurationOptions.Parse(_config.GetConnectionString("Redis"), true);
-                return ConnectionMultiplexer.Connect(configuration);
-            });
+            services.AddDbContext<AppIdentityDbContext>(x =>
+           {
+               x.UseSqlite(_config.GetConnectionString("IdentityConnection"));
+           });
+            services.AddSingleton<IConnectionMultiplexer>(c =>
+           {
+               var configuration = ConfigurationOptions.Parse(_config.GetConnectionString("Redis"), true);
+               return ConnectionMultiplexer.Connect(configuration);
+           });
             services.AddApplicationServices();
+            services.AddIdentityServices(_config);
             services.AddSwaggerDocumentation();
+            
             services.AddCors(opt =>
               {
                   opt.AddPolicy("CorsPolicy", policy =>
@@ -70,6 +77,7 @@ namespace API
             app.UseRouting();
             app.UseStaticFiles();
             app.UseCors("CorsPolicy");
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
